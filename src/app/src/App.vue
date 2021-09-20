15fbd8bbe1fe156b90ec8aefc996d3fcfeae317f
Jordan Welzbacher <aceracerff@gmail.com>
2021-09-20 10:51:58 -0400

    added boilerplate navbar, also created README

---
+++ src/app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NavBar />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/app/src/views/Create.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="create">
+    <br>Create Tournament Page<br>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Create',
+}
+</script>
